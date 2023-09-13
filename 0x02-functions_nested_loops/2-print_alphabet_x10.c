094061c226c5f7384360bfec059201573c68361a
Skuberg <xuzamdingi@gmail.com>
2023-09-13 04:16:57 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10 - Make alphabet x10 times followed by a new line.
+ * print_alphabet_x10 - Make alphabet x10 time use a new line each time.
  * Return: void
  */
 void print_alphabet_x10(void)
